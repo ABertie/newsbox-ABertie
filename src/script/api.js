c3cbd580cba12cc0a0a1ab6b01e17a251fb0a57a
ABertie <U2439307@rts-365.dk>
2023-11-02 19:26:58 +0100

    text runs of new york times' api

--- src/script/api.js
+++ src/script/api.js
@@ -1,11 +1,38 @@
 export default (async function() {
-    let response = await fetch("https://api.nytimes.com/svc/topstories/v2/home.json?api-key=LNlTIFnb0NXHyHx2WdIRXyjml6HXIlLJ")
+    let response = await fetch("https://api.nytimes.com/svc/topstories/v2/automobiles.json?api-key=LNlTIFnb0NXHyHx2WdIRXyjml6HXIlLJ")
     let data = await response.json()
+    // console.log(data);
     
-    // console.log(data.results[0]);
-    console.log(data.results[0].title);
-    console.log(data.results[0].abstract);
-    console.log(data.results[0].section);
-    // console.log(data.results[0].subsection);
+    let Categories = ["arts", "automobiles", "books/review", "business", "fashion", "food", "health", "home", "insider", "magazine", "movies", "nyregion", "obituaries", "opinion", "politics", "realestate", "science", "sports", "sundayreview", "technology", "theater", "t-magazine", "travel", "upshot", "us", "world"]
+    
+    const MAIN = document.querySelector("main")
+
+    Categories.forEach(element => {
+        const CATEGORY = document.createElement("section")
+        const BUTTON = document.createElement("button")
+        BUTTON.innerHTML = element
+        BUTTON.addEventListener("click",     function clickHandler() {
+            console.log(CATEGORY.innerHTML);
+
+            CATEGORY.innerHTML = ""
+            CATEGORY.append(BUTTON)
+        })
+
+        if (CATEGORY.innerHTML === "") CATEGORY.append(BUTTON)
+        MAIN.append(CATEGORY)
+    });
+    
+    // console.log(data.results[0].section);
+    // console.log(data.results[0].multimedia[2].url);
+    // console.log(data.results[0].title);
+    // console.log(data.results[0].abstract);
+
+    // MAIN.innerHTML = `
+    //     <button>${data.results[0].section}</button>
+    //         <article>
+    //             <img src="${data.results[0].multimedia[2].url}">
+    //             <h1>${data.results[0].title}</h1>
+    //             <p>${data.results[0].abstract}</p>
+    //         </article>`
 
 })()
