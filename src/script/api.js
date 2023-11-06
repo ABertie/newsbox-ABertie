1e6ce05bffc4a91bc9d161880f31d2781bfe78aa
ABertie <U2439307@rts-365.dk>
2023-11-06 11:46:38 +0100

    Api working

--- src/script/api.js
+++ src/script/api.js
@@ -1,56 +1,64 @@
-import { fetchApi } from "./fetchApi";
-// export default (async function API() {
-//     let response = await fetch(`https://api.nytimes.com/svc/topstories/v2/${category}.json?api-key=LNlTIFnb0NXHyHx2WdIRXyjml6HXIlLJ`)
-//         let data = await response.json()
-//         return data
-// })()
-
 export default (async function() {
     if (!window.location.pathname.includes("index.html")) return
     
-    let categories = ["arts", "automobiles", "books/review", "business", "fashion", "food", "health", "home", "insider", "magazine", "movies", "nyregion", "obituaries", "opinion", "politics", "realestate", "science", "sports", "sundayreview", "technology", "theater", "t-magazine", "travel", "upshot", "us", "world"]
+    let categories = ["arts", "automobiles", "books", "business", "fashion", "food", "health", "home", "insider", "magazine", "movies", "nyregion", "obituaries", "opinion", "politics", "realestate", "science", "sundayreview", "technology", "theater", "t-magazine", "travel", "upshot", "us", "world"]
+    // , "sports" only makes Promo
+
+    const COUNT = 7 // count of articles for each categorie - 1
     
     const MAIN = document.querySelector("main")
     
     categories.forEach(element => {
         const CATEGORY = document.createElement("section")
         const BUTTON = document.createElement("button")
+        const ARTICLES = document.createElement("div")
         BUTTON.innerHTML = element
         BUTTON.addEventListener("click", function clickHandler() {
-            let URL = `https://api.nytimes.com/svc/topstories/v2/${element}.json?api-key=LNlTIFnb0NXHyHx2WdIRXyjml6HXIlLJ`
-            console.log(data);
             
-            if (CATEGORY.childElementCount !== 1) {
-                CATEGORY.innerHTML = ""
-                CATEGORY.append(BUTTON)
+            if (ARTICLES.innerHTML !== "") {
+                ARTICLES.innerHTML = ""
             }
             else {
-                // CATEGORY.innerHTML = `
-                // <button>${data.results[0].section}</button>
-                // <article>
-                //     <img src="${data.results[0].multimedia[2].url}">
-                //     <h1>${data.results[0].title}</h1>
-                //     <p>${data.results[0].abstract}</p>
-                //     </article>`
+                fetch(`https://api.nytimes.com/svc/topstories/v2/${element}.json?api-key=LNlTIFnb0NXHyHx2WdIRXyjml6HXIlLJ`)
+                .then(function(response) {
+                    if (response.status !== 200) 
+                        throw new Error("fejlbesked")
+                    return response.json()
+                })
+                .then(function(data) {
+                    // console.log(data);
                     
+                    data.results.forEach(object => {
+                        if (object.item_type === "Article" || object.item_type === "Interactive") {
+                            if (ARTICLES.childElementCount > COUNT) return
+
+                            if (object.multimedia) {
+                                const ARTICLE = document.createElement("article")
+                                ARTICLE.innerHTML = `
+                                <img src="${object.multimedia[2].url}">
+                                <h1>${object.title}</h1>
+                                <p>${object.abstract}</p>`
+
+                                ARTICLES.append(ARTICLE)
+                            } 
+                            else {
+                                const ARTICLE = document.createElement("article")
+                                ARTICLE.innerHTML = `
+                                <h1>${object.title}</h1>
+                                <p>${object.abstract}</p>`
+
+                                ARTICLES.append(ARTICLE)
+                            }
+                        }
+                        else return 
+                    });
+                })
                 }
             })
             
-            if (CATEGORY.innerHTML === "") CATEGORY.append(BUTTON)
+            CATEGORY.append(BUTTON)
+            CATEGORY.append(ARTICLES)
             MAIN.append(CATEGORY)
     });
-    
-    // console.log(data.results[0].section);
-    // console.log(data.results[0].multimedia[2].url);
-    // console.log(data.results[0].title);
-    // console.log(data.results[0].abstract);
-    
-    // MAIN.innerHTML = `
-    //     <button>${data.results[0].section}</button>
-    //         <article>
-    //             <img src="${data.results[0].multimedia[2].url}">
-    //             <h1>${data.results[0].title}</h1>
-    //             <p>${data.results[0].abstract}</p>
-    //         </article>`
 
 })()
--- src/script/fetchApi.js
+++
@@ -1,5 +0,0 @@
-export async function fetchApi(URL) {
-    let response = await fetch(URL)
-    let data = await response.json()
-    return data
-}
