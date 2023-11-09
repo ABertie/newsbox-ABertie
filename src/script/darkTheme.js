d2699313956ff0cf0f386d3357a39844118d77bf
ABertie <U2439307@rts-365.dk>
2023-11-09 19:21:28 +0100

    small error fiks

--- src/script/darkTheme.js
+++ src/script/darkTheme.js
@@ -1,13 +1,12 @@
 export default (function () {
     if (window.matchMedia("(prefers-color-scheme: dark)").matches 
-    && localStorage.getItem("theme") !== "") {
+    && localStorage.getItem("theme") !== "lightTheme") {
         localStorage.setItem("theme", "darkTheme")
     }
-
-    if(!localStorage.getItem("theme")) {
-        localStorage.setItem("theme", "")
+    else if(!localStorage.getItem("theme")) {
+        localStorage.setItem("theme", "lightTheme")
     } 
-
+    
     document.body.classList.add(localStorage.getItem("theme"))
     
     if (!window.location.pathname.includes("settings.html")) return
@@ -22,7 +21,7 @@ export default (function () {
         CLASS_LIST.toggle("darkTheme")
         localStorage.setItem("theme", CLASS_LIST.contains("darkTheme")
         ? "darkTheme"
-        : "")
+        : "lightTheme")
 
         BUTTON.innerHTML = `Toggle ${localStorage.getItem("theme") === "darkTheme" ? "light" : "dark"} mode`
     }
