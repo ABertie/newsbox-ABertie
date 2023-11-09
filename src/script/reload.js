ffcfb4a0e7e3595e74596c0d25b8cc88f9d00c66
ABertie <U2439307@rts-365.dk>
2023-11-09 20:16:47 +0100

    reload page working

---
+++ src/script/reload.js
@@ -0,0 +1,34 @@
+export default (function () {
+    const MAIN = document.querySelector("main")
+    MAIN.addEventListener("touchstart", touchHandler)
+    MAIN.addEventListener("touchend", touchHandler)
+    let y
+    const LOADER = document.createElement("i")
+    LOADER.classList.add("fa-solid")
+    LOADER.classList.add("fa-spinner")
+    LOADER.classList.add("fa-spin-pulse")
+            
+    function touchHandler(event) {
+        const TOUCH__EVENT = event.changedTouches[0].clientY
+        
+        if (event.type === "touchstart") {
+            y = TOUCH__EVENT
+        }
+        else if (event.type === "touchend") {
+
+            let Direction
+            const TOLERANCE = 50
+            
+            if (y + TOLERANCE < TOUCH__EVENT) Direction = "Down"
+            else Direction = null
+            
+            if(Direction) {
+                MAIN.prepend(LOADER)
+                location.reload(location.href)
+                Direction = null
+            }
+
+            y = null
+        }
+    }
+})()
